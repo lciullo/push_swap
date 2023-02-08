1120b9e13e28767b97b33843fd97b810b47a93f4
lciullo <lisa.ciullo42@gmail.com>
2023-02-08 19:19:45 +0100

    add the max shift of index because I can shift to more than 32 bits I can't use just the max index any more

--- src/sort/radix.c
+++ src/sort/radix.c
@@ -18,20 +18,32 @@ static int get_max_index(t_list **a)
     return (max);
 }
 
-void radix_sort(t_list **a, t_list **b, t_stack *len) 
+static int  get_highest_shift(t_list **a)
 {
     int max;
+    int shift;
+
+    max = get_max_index(a);
+    shift = 0;
+    while (max >> shift != 0)
+        shift++;
+    return (shift);
+}
+
+void radix_sort(t_list **a, t_list **b, t_stack *len) 
+{
+    int max_shift;
     int i;
     int j;
     t_list *head;
   
-    max = get_max_index(a);
+    max_shift = get_highest_shift(a);
     i = 0;
     head = *a;
-    while (i < max)
+    while (i < max_shift)//|| is_sorted(a) == 1 ? voir si on peut raccoursir demain
     {
         j = 0;
-        while (j < len->stack_a)
+        while (j != len->stack_a)
         {
             head = *a;
             if ((head->index >> i) % 2 == 1)
@@ -41,9 +53,11 @@ void radix_sort(t_list **a, t_list **b, t_stack *len)
             else
                 push_b(b, a);
             j++;
+            ft_printf("j %d\n", j);
         }
         while (ft_lstsize(*b) != 0)
 			push_a(a, b);
         i++;
+        ft_printf("i %d\n", i);
     }
 }
